0500bb8699fc5bfcb98b4db39b687bfc224374d0
IvanProkhorets <prohorets.ivan@gmail.com>
2022-07-20 23:14:46 +0300

    comleted task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+const NOTIFICATION_DELAY = 1000;
+let interval = null;
+
+refs.start.addEventListener('click', onButtonStart);
+refs.stop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+  refs.start.disabled = true;
+
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, NOTIFICATION_DELAY);
+}
+
+function onButtonStop() {
+  refs.start.disabled = false;
+  clearInterval(interval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
