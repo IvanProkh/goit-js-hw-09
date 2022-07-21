69f04ea26ec94c342314b2f2ce004ce1c147f6e1
IvanProkhorets <prohorets.ivan@gmail.com>
2022-07-21 15:21:54 +0300

    update task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,10 +1,21 @@
-// *Напиши скрипт таймера, который ведёт обратный отсчет до определенной даты.Такой таймер может использоваться в блогах и интернет - магазинах, страницах регистрации событий, во время технического обслуживания и т.д.Посмотри демо видео работы таймера.
-
 // *В HTML есть готовая разметка таймера, поля выбора конечной даты и кнопки, при клике по которой таймер должен запускаться.Добавь минимальное оформление элементов интерфейса.
 
 // *Используй библиотеку flatpickr для того чтобы позволить пользователю кроссбраузерно выбрать конечную дату и время в одном элементе интерфейса. Для того чтобы подключить CSS код библиотеки в проект, необходимо добавить еще один импорт, кроме того который описан в документации.
 
+// *Если пользователь выбрал дату в прошлом, покажи window.alert() с текстом "Please choose a date in the future".
+// *Если пользователь выбрал валидную дату (в будущем), кнопка «Start» становится активной.
+// *Кнопка «Start» должа быть не активна до тех пор, пока пользователь не выбрал дату в будущем.
+// *При нажатии на кнопку «Start» начинается отсчет времени до выбранной даты с момента нажатия.
+
+// *При нажатии на кнопку «Start» скрипт должен вычислять раз в секунду сколько времени осталось до указанной даты и обновлять интерфейс таймера, показывая четыре цифры: дни, часы, минуты и секунды в формате xx:xx:xx:xx.
+
+// *Количество дней может состоять из более чем двух цифр.
+// *Таймер должен останавливаться когда дошел до конечной даты, то есть 00:00:00:00.
+
+// *Функция convertMs() возвращает объект с рассчитанным оставшимся временем до конечной даты. Обрати внимание, что она не форматирует результат. То есть, если осталось 4 минуты или любой другой составляющей времени, то функция вернет 4, а не 04. В интерфейсе таймера необходимо добавлять 0 если в числе меньше двух символов. Напиши функцию addLeadingZero(value), которая использует метод метод padStart() и перед отрисовкой интефрейса форматируй значение.
+
 import flatpickr from 'flatpickr';
+import Notiflix from 'notiflix';
 
 import 'flatpickr/dist/flatpickr.min.css';
 
@@ -17,3 +28,86 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+flatpickr('#datetime-picker', options);
+
+const refs = {
+  picker: document.querySelector('#datetime-picker'),
+  timer: document.querySelector('.timer'),
+  start: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.timer.style.display = 'flex';
+refs.timer.style.flexDirection = 'row';
+refs.timer.style.justifyContent = 'flex-start';
+refs.timer.style.alignItems = 'center';
+
+console.dir(refs.timer);
+
+refs.start.addEventListener('click', onButtonStart);
+refs.picker.addEventListener('input', checkTime);
+
+refs.start.disabled = true;
+
+function checkTime() {
+  const date = new Date(document.querySelector('#datetime-picker').value);
+  const now = new Date();
+
+  if (date < now) {
+    Notiflix.Notify.warning('Please choose a date in the future');
+    refs.start.disabled = true;
+    return;
+  }
+  refs.start.disabled = false;
+}
+
+function addLeadingZero(value) {
+  const value = String(this).padStart(2, '0');
+  return value;
+}
+
+function onButtonStart() {
+  console.log(refs.picker.value);
+
+  const date = new Date(refs.picker.value);
+  const now = new Date();
+  let diff = date - now;
+  console.log('~ diff', diff);
+  console.log(convertMs(diff));
+  convertMs(diff);
+
+  setInterval(() => {
+    if (diff <= 0) {
+      clearInterval();
+      return;
+    }
+    diff -= 1000;
+    refs.days.textContent = addLeadingZero(convertMs(diff).days);
+    refs.hours.textContent = addLeadingZero(convertMs(diff).hours);
+    refs.minutes.textContent = addLeadingZero(convertMs(diff).minutes);
+    refs.seconds.textContent = addLeadingZero(convertMs(diff).seconds);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
